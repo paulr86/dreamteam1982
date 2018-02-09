81780be799742c6e8cdd593dcf8b48c110aa7fcb
Ruben <rubentijeras44@gmail.com>
2018-02-09 16:38:42 +0100

    asdf

--- DreamTeam1982/src/Alumno.java
+++ DreamTeam1982/src/Alumno.java
@@ -1,4 +1,20 @@
 
 public class Alumno {
 
+	private String dni;
+	private String nombre;
+	private String apellidos;
+	private String telefono;
+	private String email;
+	private ArrayList <DiaClase> faltas = new ArrayList <DiaClase>();
+	private ArrayList <Calificacion> notas = new ArrayList <Calificacion>();
+	
+	//Constructores
+	
+	public Alumno (String dni) {
+		this.dni = dni;
+		this.faltas = new Diaclase();
+		this.notas = Calificacion.getAsignatura()+" "+Calificacion.getNota();
+	}
+	
 }
