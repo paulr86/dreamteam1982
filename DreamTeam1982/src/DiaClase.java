da17e72f1a89d8063323620ccca465c77e97974f
alvaro <alvaro_mp8@hotmail.com>
2018-02-09 11:55:32 +0100

    update DiaClase

--- DreamTeam1982/src/DiaClase.java
+++ DreamTeam1982/src/DiaClase.java
@@ -1,4 +1,42 @@
+package gestionAlumnos_ProyectoGrupal;
 
-public class Dia_Clase {
-
+public class DiaClase {
+	
+	// Atributos de la clase
+	
+	private Fecha dia;
+	private ArrayList<Horario> sesiones;
+	
+	// Inicializa el día y crea la instancia de horario
+	
+	public DiaClase(Fecha dia) {
+		this.dia = dia;
+		this.sesiones = new ArrayList<Horario>();
+	}
+	
+	// Getter y set de 'dia'
+	
+	public Fecha getDia() {
+		return dia;
+	}
+	
+	public Horario getSesiones() {
+		return sesiones;
+	}
+	
+	public void setDia(Fecha dia) {
+		this.dia = dia;
+	}
+	
+	// Sobrecarga del método equals de la clase Object
+	
+	public boolean equals(Object object){
+		boolean igual = false;
+		if(object instanceof DiaClase){
+			DiaClase diaClase = (DiaClase) object;
+			if(diaClase.getDia().equals(this.getDia()))
+				igual = true;
+		}
+		return igual;
+	}
 }
