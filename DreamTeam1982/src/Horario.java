c0b002566156f32f9acb9e9f93552d1196acbc01
Ignacio <gtignacio@hotmail.com>
2018-02-13 19:23:57 +0100

    Horario

--- DreamTeam1982/src/Horario.java
+++ DreamTeam1982/src/Horario.java
@@ -1,4 +1,35 @@
 
 public class Horario {
 
+	private char[] sesiones = new char [6];
+
+//Constructor por defeco
+		
+		public Horario (){
+			for (int y = 0; y < 6; y++) {
+			sesiones[y] = ' ';
+			}
+		}
+//GetSesiones
+			
+		public char[] GetSesiones() {
+			return sesiones;
+		}	
+		
+//faltaDiaEntero()
+
+		public void faltaDiaEntero() {
+			for (int y = 0; y < 6; y++) {
+				sesiones[y] = 'F';
+			}
+		}
+		
+//faltaHora (int sesion)
+
+		public void faltaHora (int sesion) {
+			sesiones[sesion - 1] = 'F';
+			
+			
+		// de 1 a 6 hora 
+	}
 }
